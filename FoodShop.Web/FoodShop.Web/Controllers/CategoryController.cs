d82ca1c9c0523ca2f9c98545eec40809bcaa5672
Silvia Todorova <164194780+SilviaTodorova7@users.noreply.github.com>
2024-04-19 16:50:21 +0300

    Category All added
    Category All added

---
+++ FoodShop.Web/FoodShop.Web/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using FoodShop.Services.Interfaces;
+using FoodShop.Web.ViewModels.Category;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodShop.Web.Controllers
+{
+    [Authorize]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+        public async Task<IActionResult> All()
+        {
+            ICollection<CategoryViewModel> allCategories =
+                await this.categoryService.GetAllCategoriesAsync();
+
+            return View(allCategories);
+        }
+    }
+}
